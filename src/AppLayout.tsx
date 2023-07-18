8a61cfbdf505db69c9e8e91f04390956437967ee
saki <sakihet@gmail.com>
2023-07-18 21:43:21 +0900

    update style

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -9,7 +9,7 @@ export function AppLayout(props: AppLayoutProps) {
     <div class="p-4">
       <div class="h-12">
         <Link href="/" class="text-decoration-none">
-          <h1 class="text-center text-large">DnD Board (WIP)</h1>
+          <h1 class="m-0 text-center text-large">DnD Board (WIP)</h1>
         </Link>
       </div>
       <div>{props.children}</div>
