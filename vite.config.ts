7e2c388a0b0fee7f1340f7ba24953673499dba3f
saki <sakihet@gmail.com>
2023-08-13 23:11:49 +0900

    add pwa config to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    VitePWA({registerType: 'autoUpdate'})
+  ],
 })
