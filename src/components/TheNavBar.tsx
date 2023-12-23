759838f4208aae93fe33e7dec3667187bbbeabb4
saki <sakihet@gmail.com>
2023-12-23 14:34:03 +0900

    update markup

--- src/components/TheNavBar.tsx
+++ src/components/TheNavBar.tsx
@@ -16,32 +16,36 @@ export default function TheNavBar({ theme }: { theme: Signal<Theme> }) {
   }
 
   return (
-    <div class="h-12 p-3 flex-row bg-primary border-0 border-b-1 border-solid border-color-primary">
-      <div class="f-1 h-6"></div>
-      <div class="w-24 h-6">
-        <Link
-          href="/"
-          class="text-decoration-none h-6"
-        >
-          <h1 class="m-0 h-6 text-center text-medium text-primary font-weight-600 hover-bg-link cursor-pointer">
-            Trellith
-          </h1>
-        </Link>
-      </div>
-      <div class="f-1 h-6 text-right">
-        <div class="layout-stack-horizontal-1">
-          <Link
-            href="/about"
-            class="text-decoration-none text-secondary text-small hover px-2 py-1"
-          >
-            About
-          </Link>
-          <AppButton
-            text={theme.value === 'light' ? 'Light' : 'Dark'}
-            onClick={handleClick}
-          />
+    <header>
+      <nav>
+        <div class="h-12 p-3 flex-row bg-primary border-0 border-b-1 border-solid border-color-primary">
+          <div class="f-1 h-6"></div>
+          <div class="w-24 h-6">
+            <Link
+              href="/"
+              class="text-decoration-none h-6"
+            >
+              <h1 class="m-0 h-6 text-center text-medium text-primary font-weight-600 hover-bg-link cursor-pointer">
+                Trellith
+              </h1>
+            </Link>
+          </div>
+          <div class="f-1 h-6 text-right">
+            <div class="layout-stack-horizontal-1">
+              <Link
+                href="/about"
+                class="text-decoration-none text-secondary text-small hover px-2 py-1"
+              >
+                About
+              </Link>
+              <AppButton
+                text={theme.value === 'light' ? 'Light' : 'Dark'}
+                onClick={handleClick}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </nav>
+    </header>
   )
 }
