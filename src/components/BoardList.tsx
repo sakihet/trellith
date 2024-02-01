44693d9b8bc559759344f9fded9c0b45eba87738
saki <sakihet@gmail.com>
2024-02-02 08:59:00 +0900

    add filtering boards feature

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -1,6 +1,7 @@
 import { JSX } from "preact/jsx-runtime"
 import { Board } from "../types/board"
 import BoardItem from "./BoardItem"
+import { useState } from "preact/hooks"
 
 export default function BoardList(
   {
@@ -19,8 +20,21 @@ export default function BoardList(
     handleToggleDialog: (e: JSX.TargetedMouseEvent<HTMLButtonElement>) => void
   }
 ) {
+  const [query, setQuery] = useState('')
+
+  const handleSearch = (e: JSX.TargetedMouseEvent<HTMLInputElement>) => {
+    setQuery(e.currentTarget.value)
+  }
+
   return (
-    <div class="layout-stack-2 overflow-y-auto pattern-height-board-list py-2 pr-2 pattern-scrollbar-thick">
+    <div class="layout-stack-4 overflow-y-auto pattern-height-board-list py-2 pr-2 pattern-scrollbar-thick">
+      <input
+        type="search"
+        class="h-6 w-48 px-2 border-solid border-1 border-color-primary bg-primary"
+        placeholder="Filter"
+        onSearch={handleSearch}
+        disabled={boards.length === 0}
+      />
       {boards.length === 0
         ?
         <button
@@ -29,18 +43,22 @@ export default function BoardList(
           onClick={handleToggleDialog}
         >Create new board</button>
         :
-        boards.map((board, idx) =>
-          <BoardItem
-            key={board.id}
-            board={board}
-            pos={idx === 0 ? "first" : (idx === (boards.length - 1) ? "last" : "middle")}
-            cardsNum={board.lists.map(l => l.cards.length).reduce((accumulator, value) => { return accumulator + value }, 0)}
-            handleDragEnd={handleDragEnd}
-            handleDragOver={handleDragOver}
-            handleDragStart={handleDragStart}
-            handleDrop={handleDrop}
-          />
-        )
+        <div class="layout-stack-2">
+          {
+            boards.filter(b => b.name.includes(query)).map((board, idx) =>
+              <BoardItem
+                key={board.id}
+                board={board}
+                pos={idx === 0 ? "first" : (idx === (boards.length - 1) ? "last" : "middle")}
+                cardsNum={board.lists.map(l => l.cards.length).reduce((accumulator, value) => { return accumulator + value }, 0)}
+                handleDragEnd={handleDragEnd}
+                handleDragOver={handleDragOver}
+                handleDragStart={handleDragStart}
+                handleDrop={handleDrop}
+              />
+            )
+          }
+        </div>
       }
     </div>
   )
