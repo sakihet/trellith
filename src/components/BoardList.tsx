3d60d65e0b0261069dd4f29e73bfa923a031a214
saki <sakihet@gmail.com>
2024-02-08 01:02:05 +0900

    update board filter form

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -2,6 +2,7 @@ import { JSX } from "preact/jsx-runtime"
 import { Board } from "../types/board"
 import BoardItem from "./BoardItem"
 import { useState } from "preact/hooks"
+import IconFilterList from "./IconFilterList"
 
 export default function BoardList(
   {
@@ -28,13 +29,21 @@ export default function BoardList(
 
   return (
     <div class="layout-stack-4 overflow-y-auto pattern-height-board-list py-2 pr-2 pattern-scrollbar-thick">
-      <input
-        type="search"
-        class="h-6 w-48 px-2 border-solid border-1 border-color-primary bg-primary"
-        placeholder="Filter"
-        onSearch={handleSearch}
-        disabled={boards.length === 0}
-      />
+      <div class="flex-row">
+        <label for="board-filter">
+          <div class="inline-block h-6 w-6 text-center border-solid border-1 border-color-primary">
+            <IconFilterList />
+          </div>
+        </label>
+        <input
+          id="board-filter"
+          type="search"
+          class="w-48 h-6 px-2 bg-primary border-solid border-1 border-color-primary border-l-none"
+          placeholder="Filter"
+          onSearch={handleSearch}
+          disabled={boards.length === 0}
+        />
+      </div>
       {boards.length === 0
         ?
         <button
